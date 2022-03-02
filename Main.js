993cec92d4210adeaf9963acfce089f0b2b2be40
unknown <tpatel62@nyit.edu>
2022-03-02 02:06:31 -0500

    Changed SignIn Page AND Home page

---
+++ Main.js
@@ -0,0 +1,25 @@
+// let d = new Date();
+// alert("Today's date is " + d);
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("topnav");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+document.getElementById("btnSignUp").onclick = function () {
+    location.href = "SignUpPage.html";
+};
+
